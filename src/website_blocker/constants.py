bf31645013ca22ed6ec8f467f7132277b54d8e02
Bishwa Saha <77796630+kun-codes@users.noreply.github.com>
2025-06-01 09:42:20 +0530

    fix(mitmdump): handle spaces in paths for mitmdump command execution

--- src/website_blocker/constants.py
+++ src/website_blocker/constants.py
@@ -10,7 +10,7 @@ MITMDUMP_SHUTDOWN_URL = f"http://shutdown.{APPLICATION_NAME.lower()}.internal/"
 BLOCK_HTML_MESSAGE = f"<h1>Website blocked by {APPLICATION_NAME}!</h1>"
 
 MITMDUMP_COMMAND_LINUX = (
-    '{} --set allow_remote=true -p {} --showhost -s {} --set "addresses_str={}" --set "block_type={}"'.format(
+    '"{}" --set allow_remote=true -p {} --showhost -s "{}" --set "addresses_str={}" --set "block_type={}"'.format(
         "{}",
         "{}",
         os.path.join(getattr(sys, "_MEIPASS", Path(__file__).parent), "filter.py"),
@@ -21,7 +21,7 @@ MITMDUMP_COMMAND_LINUX = (
 # the os.path.join returns the location of filter.py
 
 MITMDUMP_COMMAND_WINDOWS = (
-    r"{} --set allow_remote=true -p {} --showhost -s {} --set addresses_str={} --set block_type={}".format(
+    r'"{}" --set allow_remote=true -p {} --showhost -s "{}" --set "addresses_str={}" --set "block_type={}"'.format(
         "{}",
         "{}",
         os.path.join(getattr(sys, "_MEIPASS", Path(__file__).parent), "filter.py"),
