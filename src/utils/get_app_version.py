694aa9fdcaacfd4c7025ea25a109170a878f2ed9
Bishwa Saha <77796630+kun-codes@users.noreply.github.com>
2025-06-24 12:11:29 +0530

    chore(get_app_version.py): add comment about automatic updates from PyCharm

--- src/utils/get_app_version.py
+++ src/utils/get_app_version.py
@@ -1,4 +1,6 @@
 # if edited, update regex in ../../file-watchers/sync-version.py
 
+# this file is automatically updated by the file watcher in PyCharm when pyproject.toml is modified
+
 def get_app_version():
     return "0.1.0"
