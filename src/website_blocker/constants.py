25b2536c4b19eb968271098895971c369b56dab1
Bishwa Saha <77796630+kun-codes@users.noreply.github.com>
2025-06-25 19:11:57 +0530

    fix(constants.py): update shutdown URL for mitmdump in flatpak environment so mitmdump can shutdown gracefully

--- src/website_blocker/constants.py
+++ src/website_blocker/constants.py
@@ -1,6 +1,14 @@
 # Don't add any 3rd party imports here, as this file is used by mitmdump directly through filter.py
 
+import os
+import platform
+
 APPLICATION_NAME = "Koncentro"
 
-MITMDUMP_SHUTDOWN_URL = f"http://shutdown.{APPLICATION_NAME.lower()}.internal/"
+# from: https://stackoverflow.com/a/75284996
+if platform.system() == 'Linux' and os.environ.get('container') is not None:  # is running in flatpak
+    MITMDUMP_SHUTDOWN_URL = "http://127.0.0.1:8080/shutdown-mitmdump"  # inside flatpak, dns resolution is sandboxed
+else:
+    MITMDUMP_SHUTDOWN_URL = f"http://shutdown.{APPLICATION_NAME.lower()}.internal/"
+
 BLOCK_HTML_MESSAGE = f"<h1>Website blocked by {APPLICATION_NAME}!</h1>"
