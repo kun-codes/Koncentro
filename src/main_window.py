4734c9b78e569bd5cda1d882545ff75cd4cc8f08
Bishwa Saha <77796630+kun-codes@users.noreply.github.com>
2025-08-14 14:15:44 +0530

    refactor(main_window.py, systemTray.py): refactor system tray functionality into a dedicated `SystemTray` class

--- src/main_window.py
+++ src/main_window.py
@@ -1,4 +1,3 @@
-import os
 import platform
 import socket
 import threading
@@ -6,8 +5,8 @@ from pathlib import Path
 
 from loguru import logger
 from PySide6.QtCore import QModelIndex, QSize, Qt
-from PySide6.QtGui import QAction, QFont, QIcon, QKeySequence, QShortcut
-from PySide6.QtWidgets import QApplication, QMenu, QSystemTrayIcon
+from PySide6.QtGui import QFont, QIcon, QKeySequence, QShortcut
+from PySide6.QtWidgets import QApplication
 from qfluentwidgets import (
     FluentIcon,
     InfoBar,
@@ -16,8 +15,6 @@ from qfluentwidgets import (
     NavigationItemPosition,
     PushButton,
     SystemThemeListener,
-    Theme,
-    qconfig,
 )
 
 from config_paths import settings_dir
@@ -38,6 +35,7 @@ from models.task_list_model import TaskListModel
 from models.workspace_list_model import WorkspaceListModel
 from prefabs.customFluentIcon import CustomFluentIcon
 from prefabs.koncentroFluentWindow import KoncentroFluentWindow
+from prefabs.systemTray import SystemTray
 from resources import logos_rc
 from tutorial.pomodoroInterfaceTutorial import PomodoroInterfaceTutorial
 from tutorial.taskInterfaceTutorial import TaskInterfaceTutorial
@@ -45,7 +43,6 @@ from tutorial.websiteBlockerInterfaceTutorial import WebsiteBlockerInterfaceTuto
 from tutorial.workspaceManagerDialogTutorial import WorkspaceManagerDialogTutorial
 from utils.check_for_updates import UpdateChecker
 from utils.check_internet_worker import CheckInternetWorker
-from utils.detect_windows_version import isWin10OrEarlier
 from utils.find_mitmdump_executable import get_mitmdump_path
 from utils.isMitmdumpRunning import isMitmdumpRunningWorker
 from utils.time_conversion import convert_ms_to_hh_mm_ss
@@ -103,7 +100,7 @@ class MainWindow(KoncentroFluentWindow):
 
         self.initNavigation()
         self.initWindow()
-        self.initSystemTray()
+        self.system_tray = SystemTray(self)
         self.initBottomBar()
         self.connectSignalsToSlots()
 
@@ -151,136 +148,10 @@ class MainWindow(KoncentroFluentWindow):
 
         self.setMicaEffectEnabled(app_settings.get(app_settings.mica_enabled))
 
-    def initSystemTray(self) -> None:
-        """Initialize system tray icon and notifications"""
-        self.tray = QSystemTrayIcon(self)
-
-        self.tray_menu = QMenu()
-
-        is_os_dark_mode = qconfig.theme == Theme.DARK
-        desktop = os.environ.get("XDG_CURRENT_DESKTOP", "").lower()
-        is_gnome = "gnome" in desktop
-
-        self.tray_menu_timer_status_action = self.tray_menu.addAction("Timer not running")
-        self.tray_menu_timer_status_action.setIcon(
-            FluentIcon.STOP_WATCH.icon(Theme.DARK if is_os_dark_mode else Theme.LIGHT)
-        )
-        self.tray_menu_timer_status_action.setEnabled(False)  # Make it non-clickable
-
-        self.tray_menu.addSeparator()
-
-        # Timer control actions
-        # context menu of Windows 10 system tray icon is always in light mode for qt apps.
-        self.tray_menu_start_action = self.tray_menu.addAction("Start")
-        dark_mode_condition: bool = is_os_dark_mode and not isWin10OrEarlier()
-        self.tray_menu_start_action.setIcon(FluentIcon.PLAY.icon(Theme.DARK if dark_mode_condition else Theme.LIGHT))
-        self.tray_menu_start_action.triggered.connect(lambda: self.pomodoro_interface.pauseResumeButton.click())
-
-        self.tray_menu_pause_resume_action = self.tray_menu.addAction("Pause/Resume")
-        self.tray_menu_pause_resume_action.setIcon(
-            CustomFluentIcon.PLAY_PAUSE.icon(Theme.DARK if dark_mode_condition else Theme.LIGHT)
-        )
-        self.tray_menu_pause_resume_action.triggered.connect(lambda: self.pomodoro_interface.pauseResumeButton.click())
-        self.tray_menu_pause_resume_action.setEnabled(False)
-
-        self.tray_menu_stop_action = self.tray_menu.addAction("Stop")
-        self.tray_menu_stop_action.setIcon(FluentIcon.CLOSE.icon(Theme.DARK if dark_mode_condition else Theme.LIGHT))
-        self.tray_menu_stop_action.triggered.connect(lambda: self.pomodoro_interface.stopButton.click())
-
-        self.tray_menu_skip_action = self.tray_menu.addAction("Skip")
-        self.tray_menu_skip_action.setIcon(
-            FluentIcon.CHEVRON_RIGHT.icon(Theme.DARK if dark_mode_condition else Theme.LIGHT)
-        )
-        self.tray_menu_skip_action.triggered.connect(lambda: self.pomodoro_interface.skipButton.click())
-
-        self.tray_menu.addSeparator()
-
-        # not adding tray_menu_show_hide_action here, it will be done in onShouldMinimizeToSystemTraySettingChanged
-        self.tray_menu_show_hide_action = QAction("Show/Hide")
-        self.tray_menu_show_hide_action.setIcon(
-            FluentIcon.VIEW.icon(Theme.DARK if dark_mode_condition else Theme.LIGHT)
-        )
-        self.tray_menu_after_show_hide_separator = QAction()
-        self.tray_menu_after_show_hide_separator.setSeparator(True)
-        self.tray_menu_show_hide_action.triggered.connect(self.toggleWindowVisibility)
-
-        self.tray_menu_quit_action = self.tray_menu.addAction("Quit")
-        self.tray_menu_quit_action.setIcon(
-            CustomFluentIcon.EXIT.icon(Theme.DARK if dark_mode_condition else Theme.LIGHT)
-        )
-        self.tray_menu_quit_action.triggered.connect(self.quitApplicationWithCleanup)
-
-        # onShouldMinimizeToSystemTraySettingChanged() adds self.tray_menu_show_hide_action to the tray menu and
-        # related separators. Also connects/disconnects the tray icon activation signal to # onSystemTrayActivated()
-        # calling onShouldMinimizeToSystemTraySettingChanged() manually as connectSignalsToSlots() is called after
-        # initSystemTray() in __init__()
-        # also calling after self.tray_menu_quit_action is created as it is used in
-        # onShouldMinimizeToSystemTraySettingChanged()
-        self.onShouldMinimizeToSystemTraySettingChanged(ConfigValues.SHOULD_MINIMIZE_TO_TRAY)
-
-        self.tray.setContextMenu(self.tray_menu)
-
-        self.tray_white_icon = QIcon(":/logosPrefix/logos/logo-monochrome-white.svg")
-        self.tray_black_icon = QIcon(":/logosPrefix/logos/logo-monochrome-black.svg")
-
-        if is_gnome:
-            initial_icon = self.tray_white_icon
-        elif is_os_dark_mode:
-            initial_icon = self.tray_white_icon
-        else:
-            initial_icon = self.tray_black_icon
-
-        self.tray.setIcon(initial_icon)
-        self.tray.setVisible(True)
-
-    def onSystemTrayActivated(self, reason: QSystemTrayIcon.ActivationReason) -> None:
-        if reason == QSystemTrayIcon.ActivationReason.Trigger:  # single left click
-            self.toggleWindowVisibility()
-
     def remainingFontSubstitutions(self) -> None:
         # This was unaffected by font substitution in __main__.py
         font = QFont("Selawik", 14)
         self.pomodoro_interface.ProgressRing.setFont(font)
-
-    def updateSystemTrayIcon(self) -> None:
-        logger.debug("Updating system tray icon")
-        # context menu of Windows 10 system tray icon is always in light mode for qt apps.
-        desktop = os.environ.get("XDG_CURRENT_DESKTOP", "").lower()
-        is_gnome = "gnome" in desktop
-        if is_gnome:
-            self.tray.setIcon(self.tray_white_icon)
-        elif qconfig.theme == Theme.DARK:
-            self.tray.setIcon(self.tray_white_icon)
-        else:
-            self.tray.setIcon(self.tray_black_icon)
-
-        if isWin10OrEarlier():
-            return
-
-        if qconfig.theme == Theme.DARK:
-            self.tray_menu_timer_status_action.setIcon(FluentIcon.STOP_WATCH.icon(Theme.DARK))
-            self.tray_menu_start_action.setIcon(FluentIcon.PLAY.icon(Theme.DARK))
-            self.tray_menu_pause_resume_action.setIcon(CustomFluentIcon.PLAY_PAUSE.icon(Theme.DARK))
-            self.tray_menu_stop_action.setIcon(FluentIcon.CLOSE.icon(Theme.DARK))
-            self.tray_menu_skip_action.setIcon(FluentIcon.CHEVRON_RIGHT.icon(Theme.DARK))
-            self.tray_menu_show_hide_action.setIcon(FluentIcon.VIEW.icon(Theme.DARK))
-            self.tray_menu_quit_action.setIcon(CustomFluentIcon.EXIT.icon(Theme.DARK))
-        else:
-            self.tray_menu_timer_status_action.setIcon(FluentIcon.STOP_WATCH.icon(Theme.LIGHT))
-            self.tray_menu_start_action.setIcon(FluentIcon.PLAY.icon(Theme.LIGHT))
-            self.tray_menu_pause_resume_action.setIcon(CustomFluentIcon.PLAY_PAUSE.icon(Theme.LIGHT))
-            self.tray_menu_stop_action.setIcon(FluentIcon.CLOSE.icon(Theme.LIGHT))
-            self.tray_menu_skip_action.setIcon(FluentIcon.CHEVRON_RIGHT.icon(Theme.LIGHT))
-            self.tray_menu_show_hide_action.setIcon(FluentIcon.VIEW.icon(Theme.LIGHT))
-            self.tray_menu_quit_action.setIcon(CustomFluentIcon.EXIT.icon(Theme.LIGHT))
-
-    def updateSystemTrayActions(self, timerState) -> None:
-        if timerState in [TimerState.WORK, TimerState.BREAK, TimerState.LONG_BREAK]:
-            self.tray_menu_pause_resume_action.setEnabled(True)
-            self.tray_menu_start_action.setEnabled(False)
-        else:
-            self.tray_menu_pause_resume_action.setEnabled(False)
-            self.tray_menu_start_action.setEnabled(True)
 
     # below 2 methods are for the bottom bar
     def initBottomBar(self) -> None:
@@ -307,54 +178,6 @@ class MainWindow(KoncentroFluentWindow):
             self.bottomBar.pauseResumeButton.setIcon(FluentIcon.PLAY)
         else:
             self.bottomBar.pauseResumeButton.setIcon(FluentIcon.PAUSE)
-
-    def showNotifications(self, timerState, isSkipped) -> None:
-        title = ""
-        message = ""
-
-        work_duration = ConfigValues.WORK_DURATION
-        break_duration = ConfigValues.BREAK_DURATION
-        long_break_duration = ConfigValues.LONG_BREAK_DURATION
-
-        if timerState == TimerState.WORK:
-            if ConfigValues.AUTOSTART_WORK or (not ConfigValues.AUTOSTART_WORK and isSkipped):
-                # if ConfigValues.AUTOSTART_WORK:
-                title = "Work Session Started"
-                message = f"{work_duration}m work session has started"
-            elif not ConfigValues.AUTOSTART_WORK and not self.pomodoro_interface.isInitialWorkSession():
-                # no point in showing that break has ended when work session is for the first time
-                if self.pomodoro_interface.pomodoro_timer_obj.getSessionProgress() == 0.5:  # long break ended
-                    title = "Long Break Ended"
-                    message = "Long break has ended. Start the next work session"
-                else:  # break ended
-                    title = "Break Ended"
-                    message = "Break has ended. Start the next work session"
-        elif timerState in [TimerState.BREAK, TimerState.LONG_BREAK]:
-            if ConfigValues.AUTOSTART_BREAK or (not ConfigValues.AUTOSTART_BREAK and isSkipped):
-                if self.pomodoro_interface.pomodoro_timer_obj.getSessionProgress() == ConfigValues.WORK_INTERVALS:
-                    title = "Long Break Session Started"
-                    message = f"{long_break_duration}m long break session has started"
-                else:
-                    title = "Break Session Started"
-                    message = f"{break_duration}m break session has started"
-            else:
-                if self.pomodoro_interface.pomodoro_timer_obj.getSessionProgress() == ConfigValues.WORK_INTERVALS:
-                    title = "Work Session Ended"
-                    message = "Work session has ended. Start the next long break session"
-                else:
-                    title = "Work Session Ended"
-                    message = "Work session has ended. Start the next break session"
-        elif timerState == TimerState.NOTHING:
-            title = "Timer Stopped"
-            message = "Timer has stopped"
-
-        if title and message:
-            self.tray.showMessage(
-                title,
-                message,
-                QSystemTrayIcon.MessageIcon.Information,
-                5000,
-            )
 
     def onWorkspaceManagerClicked(self) -> None:
         if self.manage_workspace_dialog is None:
@@ -664,7 +487,6 @@ class MainWindow(KoncentroFluentWindow):
         workspace_specific_settings.enable_website_blocker.valueChanged.connect(
             lambda: self.handle_website_blocker_settings_change()
         )
-        app_settings.should_minimize_to_tray.valueChanged.connect(self.onShouldMinimizeToSystemTraySettingChanged)
         self.stackedWidget.mousePressEvent = self.onStackedWidgetClicked
         self.settings_interface.proxy_port_card.valueChanged.connect(self.update_proxy_port)
 
@@ -674,11 +496,19 @@ class MainWindow(KoncentroFluentWindow):
             )
         )
         self.task_interface.todoTasksList.model().dataChanged.connect(self.updateBottomBarTaskLabel)
-        self.themeListener.systemThemeChanged.connect(self.updateSystemTrayIcon)
         # for system tray
-        self.pomodoro_interface.pomodoro_timer_obj.timerStateChangedSignal.connect(self.updateSystemTrayActions)
-        # for notifications
-        self.pomodoro_interface.pomodoro_timer_obj.timerStateChangedSignal.connect(self.showNotifications)
+        app_settings.should_minimize_to_tray.valueChanged.connect(
+            self.system_tray.onShouldMinimizeToSystemTraySettingChanged
+        )
+        self.themeListener.systemThemeChanged.connect(self.system_tray.updateSystemTrayIcon)
+        self.pomodoro_interface.pomodoro_timer_obj.timerStateChangedSignal.connect(
+            self.system_tray.updateSystemTrayActions
+        )
+        ## for notifications
+        self.pomodoro_interface.pomodoro_timer_obj.timerStateChangedSignal.connect(self.system_tray.showNotifications)
+        self.system_tray.connectSignalsToSlots(
+            self.pomodoro_interface, self.quitApplicationWithCleanup, self.toggleWindowVisibility
+        )
 
         self.stackedWidget.currentChanged.connect(self.showTutorial)
 
@@ -698,18 +528,6 @@ class MainWindow(KoncentroFluentWindow):
 
         self.settings_interface.setup_app_card.clicked.connect(lambda: self.preSetupMitmproxy(False))
         self.settings_interface.reset_proxy_settings.clicked.connect(self.resetProxySettings)
-
-    def onShouldMinimizeToSystemTraySettingChanged(self, value: bool) -> None:
-        if value:
-            self.tray_menu.insertAction(self.tray_menu_quit_action, self.tray_menu_show_hide_action)
-            self.tray_menu.insertAction(self.tray_menu_quit_action, self.tray_menu_after_show_hide_separator)
-
-            self.tray.activated.connect(self.onSystemTrayActivated)
-        else:
-            self.tray_menu.removeAction(self.tray_menu_show_hide_action)
-            self.tray_menu.removeAction(self.tray_menu_after_show_hide_separator)
-
-            self.tray.activated.disconnect(self.onSystemTrayActivated)
 
     def resetProxySettings(self) -> None:
         logger.debug("Reset proxy settings button clicked")
@@ -851,7 +669,7 @@ class MainWindow(KoncentroFluentWindow):
 
             timer_text = f"{current_timer_state.value}\n{hh:02d}:{mm:02d}:{ss:02d} / {t_hh:02d}:{t_mm:02d}:{t_ss:02d}"
             self.bottomBar.timerLabel.setText(timer_text)
-            self.tray_menu_timer_status_action.setText(timer_text)
+            self.system_tray.tray_menu_timer_status_action.setText(timer_text)
 
         else:
             # timer is not running
@@ -860,7 +678,7 @@ class MainWindow(KoncentroFluentWindow):
 
             timer_text = f"Idle\n{hh:02d}:{mm:02d}:{ss:02d} / {t_hh:02d}:{t_mm:02d}:{t_ss:02d}"
             self.bottomBar.timerLabel.setText(timer_text)
-            self.tray_menu_timer_status_action.setText(timer_text)
+            self.system_tray.tray_menu_timer_status_action.setText(timer_text)
 
     def check_first_run(self) -> bool:
         settings_dir_path = Path(settings_dir)
---
+++ src/prefabs/systemTray.py
@@ -0,0 +1,215 @@
+import os
+
+from loguru import logger
+from PySide6.QtGui import QAction, QIcon
+from PySide6.QtWidgets import QMenu, QSystemTrayIcon
+from qfluentwidgets import FluentIcon, Theme, qconfig
+
+from config_values import ConfigValues
+from constants import TimerState
+from prefabs.customFluentIcon import CustomFluentIcon
+from utils.detect_windows_version import isWin10OrEarlier
+
+
+class SystemTray:
+    def __init__(self, parent):
+        self.parent = parent
+        self.tray = QSystemTrayIcon(parent)
+        self.tray_menu = QMenu()
+
+        self.tray_white_icon = QIcon(":/logosPrefix/logos/logo-monochrome-white.svg")
+        self.tray_black_icon = QIcon(":/logosPrefix/logos/logo-monochrome-black.svg")
+
+        self.initSystemTray()
+
+    def initSystemTray(self) -> None:
+        """Initialize system tray icon and notifications"""
+        is_os_dark_mode = qconfig.theme == Theme.DARK
+        desktop = os.environ.get("XDG_CURRENT_DESKTOP", "").lower()
+        is_gnome = "gnome" in desktop
+
+        self.tray_menu_timer_status_action = self.tray_menu.addAction("Timer not running")
+        self.tray_menu_timer_status_action.setIcon(
+            FluentIcon.STOP_WATCH.icon(Theme.DARK if is_os_dark_mode else Theme.LIGHT)
+        )
+        self.tray_menu_timer_status_action.setEnabled(False)  # Make it non-clickable
+
+        self.tray_menu.addSeparator()
+
+        # Timer control actions
+        # context menu of Windows 10 system tray icon is always in light mode for qt apps.
+        self.tray_menu_start_action = self.tray_menu.addAction("Start")
+        dark_mode_condition: bool = is_os_dark_mode and not isWin10OrEarlier()
+        self.tray_menu_start_action.setIcon(FluentIcon.PLAY.icon(Theme.DARK if dark_mode_condition else Theme.LIGHT))
+
+        self.tray_menu_pause_resume_action = self.tray_menu.addAction("Pause/Resume")
+        self.tray_menu_pause_resume_action.setIcon(
+            CustomFluentIcon.PLAY_PAUSE.icon(Theme.DARK if dark_mode_condition else Theme.LIGHT)
+        )
+        self.tray_menu_pause_resume_action.setEnabled(False)
+
+        self.tray_menu_stop_action = self.tray_menu.addAction("Stop")
+        self.tray_menu_stop_action.setIcon(FluentIcon.CLOSE.icon(Theme.DARK if dark_mode_condition else Theme.LIGHT))
+
+        self.tray_menu_skip_action = self.tray_menu.addAction("Skip")
+        self.tray_menu_skip_action.setIcon(
+            FluentIcon.CHEVRON_RIGHT.icon(Theme.DARK if dark_mode_condition else Theme.LIGHT)
+        )
+
+        self.tray_menu.addSeparator()
+
+        # not adding tray_menu_show_hide_action here, it will be done in onShouldMinimizeToSystemTraySettingChanged
+        self.tray_menu_show_hide_action = QAction("Show/Hide")
+        self.tray_menu_show_hide_action.setIcon(
+            FluentIcon.VIEW.icon(Theme.DARK if dark_mode_condition else Theme.LIGHT)
+        )
+        self.tray_menu_after_show_hide_separator = QAction()
+        self.tray_menu_after_show_hide_separator.setSeparator(True)
+
+        self.tray_menu_quit_action = self.tray_menu.addAction("Quit")
+        self.tray_menu_quit_action.setIcon(
+            CustomFluentIcon.EXIT.icon(Theme.DARK if dark_mode_condition else Theme.LIGHT)
+        )
+
+        # onShouldMinimizeToSystemTraySettingChanged() adds self.tray_menu_show_hide_action to the tray menu and
+        # related separators. Also connects/disconnects the tray icon activation signal to onSystemTrayActivated()
+        #
+        # calling onShouldMinimizeToSystemTraySettingChanged() manually as parent.connectSignalsToSlots() is called
+        # after initializing systemTray in parent.__init()
+        #
+        # also calling after self.tray_menu_quit_action is created as it is used in
+        # onShouldMinimizeToSystemTraySettingChanged()
+        self.onShouldMinimizeToSystemTraySettingChanged(ConfigValues.SHOULD_MINIMIZE_TO_TRAY)
+
+        self.tray.setContextMenu(self.tray_menu)
+
+        if is_gnome:
+            initial_icon = self.tray_white_icon
+        elif is_os_dark_mode:
+            initial_icon = self.tray_white_icon
+        else:
+            initial_icon = self.tray_black_icon
+
+        self.tray.setIcon(initial_icon)
+        self.tray.setVisible(True)
+
+    def onSystemTrayActivated(self, reason: QSystemTrayIcon.ActivationReason) -> None:
+        if reason == QSystemTrayIcon.ActivationReason.Trigger:  # single left click
+            self.parent.toggleWindowVisibility()
+
+    def updateSystemTrayIcon(self) -> None:
+        logger.debug("Updating system tray icon")
+        # context menu of Windows 10 system tray icon is always in light mode for qt apps.
+        desktop = os.environ.get("XDG_CURRENT_DESKTOP", "").lower()
+        is_gnome = "gnome" in desktop
+        if is_gnome:
+            self.tray.setIcon(self.tray_white_icon)
+        elif qconfig.theme == Theme.DARK:
+            self.tray.setIcon(self.tray_white_icon)
+        else:
+            self.tray.setIcon(self.tray_black_icon)
+
+        if isWin10OrEarlier():
+            return
+
+        if qconfig.theme == Theme.DARK:
+            self.tray_menu_timer_status_action.setIcon(FluentIcon.STOP_WATCH.icon(Theme.DARK))
+            self.tray_menu_start_action.setIcon(FluentIcon.PLAY.icon(Theme.DARK))
+            self.tray_menu_pause_resume_action.setIcon(CustomFluentIcon.PLAY_PAUSE.icon(Theme.DARK))
+            self.tray_menu_stop_action.setIcon(FluentIcon.CLOSE.icon(Theme.DARK))
+            self.tray_menu_skip_action.setIcon(FluentIcon.CHEVRON_RIGHT.icon(Theme.DARK))
+            self.tray_menu_show_hide_action.setIcon(FluentIcon.VIEW.icon(Theme.DARK))
+            self.tray_menu_quit_action.setIcon(CustomFluentIcon.EXIT.icon(Theme.DARK))
+        else:
+            self.tray_menu_timer_status_action.setIcon(FluentIcon.STOP_WATCH.icon(Theme.LIGHT))
+            self.tray_menu_start_action.setIcon(FluentIcon.PLAY.icon(Theme.LIGHT))
+            self.tray_menu_pause_resume_action.setIcon(CustomFluentIcon.PLAY_PAUSE.icon(Theme.LIGHT))
+            self.tray_menu_stop_action.setIcon(FluentIcon.CLOSE.icon(Theme.LIGHT))
+            self.tray_menu_skip_action.setIcon(FluentIcon.CHEVRON_RIGHT.icon(Theme.LIGHT))
+            self.tray_menu_show_hide_action.setIcon(FluentIcon.VIEW.icon(Theme.LIGHT))
+            self.tray_menu_quit_action.setIcon(CustomFluentIcon.EXIT.icon(Theme.LIGHT))
+
+    def updateSystemTrayActions(self, timerState) -> None:
+        if timerState in [TimerState.WORK, TimerState.BREAK, TimerState.LONG_BREAK]:
+            self.tray_menu_pause_resume_action.setEnabled(True)
+            self.tray_menu_start_action.setEnabled(False)
+        else:
+            self.tray_menu_pause_resume_action.setEnabled(False)
+            self.tray_menu_start_action.setEnabled(True)
+
+    def showNotifications(self, timerState, isSkipped) -> None:
+        from config_values import ConfigValues
+
+        title = ""
+        message = ""
+
+        work_duration = ConfigValues.WORK_DURATION
+        break_duration = ConfigValues.BREAK_DURATION
+        long_break_duration = ConfigValues.LONG_BREAK_DURATION
+
+        if timerState == TimerState.WORK:
+            if ConfigValues.AUTOSTART_WORK or (not ConfigValues.AUTOSTART_WORK and isSkipped):
+                # if ConfigValues.AUTOSTART_WORK:
+                title = "Work Session Started"
+                message = f"{work_duration}m work session has started"
+            elif not ConfigValues.AUTOSTART_WORK and not self.parent.pomodoro_interface.isInitialWorkSession():
+                # no point in showing that break has ended when work session is for the first time
+                if self.parent.pomodoro_interface.pomodoro_timer_obj.getSessionProgress() == 0.5:  # long break ended
+                    title = "Long Break Ended"
+                    message = "Long break has ended. Start the next work session"
+                else:  # break ended
+                    title = "Break Ended"
+                    message = "Break has ended. Start the next work session"
+        elif timerState in [TimerState.BREAK, TimerState.LONG_BREAK]:
+            if ConfigValues.AUTOSTART_BREAK or (not ConfigValues.AUTOSTART_BREAK and isSkipped):
+                if (
+                    self.parent.pomodoro_interface.pomodoro_timer_obj.getSessionProgress()
+                    == ConfigValues.WORK_INTERVALS
+                ):
+                    title = "Long Break Session Started"
+                    message = f"{long_break_duration}m long break session has started"
+                else:
+                    title = "Break Session Started"
+                    message = f"{break_duration}m break session has started"
+            else:
+                if (
+                    self.parent.pomodoro_interface.pomodoro_timer_obj.getSessionProgress()
+                    == ConfigValues.WORK_INTERVALS
+                ):
+                    title = "Work Session Ended"
+                    message = "Work session has ended. Start the next long break session"
+                else:
+                    title = "Work Session Ended"
+                    message = "Work session has ended. Start the next break session"
+        elif timerState == TimerState.NOTHING:
+            title = "Timer Stopped"
+            message = "Timer has stopped"
+
+        if title and message:
+            self.tray.showMessage(
+                title,
+                message,
+                QSystemTrayIcon.MessageIcon.Information,
+                5000,
+            )
+
+    def onShouldMinimizeToSystemTraySettingChanged(self, value: bool) -> None:
+        if value:
+            self.tray_menu.insertAction(self.tray_menu_quit_action, self.tray_menu_show_hide_action)
+            self.tray_menu.insertAction(self.tray_menu_quit_action, self.tray_menu_after_show_hide_separator)
+
+            self.tray.activated.connect(self.onSystemTrayActivated)
+        else:
+            self.tray_menu.removeAction(self.tray_menu_show_hide_action)
+            self.tray_menu.removeAction(self.tray_menu_after_show_hide_separator)
+
+            self.tray.activated.disconnect(self.onSystemTrayActivated)
+
+    def connectSignalsToSlots(self, pomodoro_interface, quit_callback, toggle_visibility_callback):
+        """Connect system tray signals to main window callbacks"""
+        self.tray_menu_start_action.triggered.connect(lambda: pomodoro_interface.pauseResumeButton.click())
+        self.tray_menu_pause_resume_action.triggered.connect(lambda: pomodoro_interface.pauseResumeButton.click())
+        self.tray_menu_stop_action.triggered.connect(lambda: pomodoro_interface.stopButton.click())
+        self.tray_menu_skip_action.triggered.connect(lambda: pomodoro_interface.skipButton.click())
+        self.tray_menu_show_hide_action.triggered.connect(toggle_visibility_callback)
+        self.tray_menu_quit_action.triggered.connect(quit_callback)
