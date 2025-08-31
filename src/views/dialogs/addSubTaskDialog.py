cd2c046c979acd5004c2429ca155335cde15acc9
Bishwa Saha <77796630+kun-codes@users.noreply.github.com>
2025-08-31 23:26:51 +0530

    fix(addSubTaskDialog.py): fix tile label of `AddSubTaskDialog` from appearing at top left of app window

--- src/views/dialogs/addSubTaskDialog.py
+++ src/views/dialogs/addSubTaskDialog.py
@@ -1,11 +1,9 @@
-from qfluentwidgets import SubtitleLabel
-
 from views.dialogs.addTaskDialog import AddTaskDialog
 
 
 class AddSubTaskDialog(AddTaskDialog):
     def __init__(self, parent=None) -> None:
         super().__init__(parent=parent)
-        self.titleLabel = SubtitleLabel("Add Subtask", self)
+        self.titleLabel.setText("Add Subtask")
         self.taskEdit.setPlaceholderText("Enter Subtask name")
         self.yesButton.setText("Add Subtask")
