84ffbdb864512c474c5c6377a57f153e3578267c
Bishwa Saha <77796630+kun-codes@users.noreply.github.com>
2025-10-13 12:43:24 +0530

    refactor(task_lookup.py): add type hints to `get_elapsed_time()` and `get_target_time()`

--- src/models/task_lookup.py
+++ src/models/task_lookup.py
@@ -1,16 +1,18 @@
+from typing import Optional
+
 from models.db_tables import Task
 from utils.db_utils import get_session
 
 
 class TaskLookup:
     @staticmethod
-    def get_elapsed_time(task_id: int):
+    def get_elapsed_time(task_id: int) -> Optional[int]:
         with get_session(is_read_only=True) as session:
             task = session.get(Task, task_id)
         return task.elapsed_time if task else None
 
     @staticmethod
-    def get_target_time(task_id: int):
+    def get_target_time(task_id: int) -> Optional[int]:
         with get_session(is_read_only=True) as session:
             task = session.get(Task, task_id)
         return task.target_time if task else None
