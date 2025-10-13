729977d2c101d75d4617c6dfb0413dcb69b719dc
Bishwa Saha <77796630+kun-codes@users.noreply.github.com>
2025-10-13 13:30:34 +0530

    refactor(workspace_lookup.py): add type hints to methods

--- src/models/workspace_lookup.py
+++ src/models/workspace_lookup.py
@@ -1,23 +1,25 @@
+from typing import Optional
+
 from models.db_tables import CurrentWorkspace, Workspace
 from utils.db_utils import get_session
 
 
 class WorkspaceLookup:
     @staticmethod
-    def get_current_workspace_id():
+    def get_current_workspace_id() -> Optional[int]:
         with get_session(is_read_only=True) as session:
             current_workspace = session.query(CurrentWorkspace).first()
         return current_workspace.current_workspace_id if current_workspace else None
 
     @staticmethod
-    def get_current_workspace_name():
+    def get_current_workspace_name() -> Optional[str]:
         current_workspace_id = WorkspaceLookup.get_current_workspace_id()
         with get_session(is_read_only=True) as session:
             workspace = session.get(Workspace, current_workspace_id)
         return workspace.workspace_name if workspace else None
 
     @staticmethod
-    def get_current_workspace():
+    def get_current_workspace() -> Optional[Workspace]:
         current_workspace_id = WorkspaceLookup.get_current_workspace_id()
         with get_session(is_read_only=True) as session:
             workspace = session.get(Workspace, current_workspace_id)
