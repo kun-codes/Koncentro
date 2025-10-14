e2dbfa9db325b327006774a8ca2bc26a9db67ce7
Bishwa Saha <77796630+kun-codes@users.noreply.github.com>
2025-10-14 11:26:36 +0530

    refactor(addSubTaskDialog.py): add type hints

--- src/views/dialogs/addSubTaskDialog.py
+++ src/views/dialogs/addSubTaskDialog.py
@@ -1,8 +1,12 @@
+from typing import Optional
+
+from PySide6.QtWidgets import QWidget
+
 from views.dialogs.addTaskDialog import AddTaskDialog
 
 
 class AddSubTaskDialog(AddTaskDialog):
-    def __init__(self, parent=None) -> None:
+    def __init__(self, parent: Optional[QWidget] = None) -> None:
         super().__init__(parent=parent)
         self.titleLabel.setText("Add Subtask")
         self.taskEdit.setPlaceholderText("Enter Subtask name")
