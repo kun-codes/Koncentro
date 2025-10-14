63b8cf4d99b86abf499641da3db8c2e5679524d2
Bishwa Saha <77796630+kun-codes@users.noreply.github.com>
2025-10-14 11:28:35 +0530

    refactor(addTaskDialog.py): add type hints

--- src/views/dialogs/addTaskDialog.py
+++ src/views/dialogs/addTaskDialog.py
@@ -1,4 +1,8 @@
+from typing import Optional
+
 from PySide6.QtCore import Qt
+from PySide6.QtGui import QKeyEvent, QShowEvent
+from PySide6.QtWidgets import QWidget
 from qfluentwidgets import LineEdit, MessageBoxBase, SubtitleLabel
 
 
@@ -7,7 +11,7 @@ class AddTaskDialog(MessageBoxBase):
     For the add task dialog in tasks view
     """
 
-    def __init__(self, parent=None) -> None:
+    def __init__(self, parent: Optional[QWidget] = None) -> None:
         super().__init__(parent=parent)
         self.titleLabel = SubtitleLabel("Add Task", self)
         self.taskEdit = LineEdit(self)
@@ -27,11 +31,11 @@ class AddTaskDialog(MessageBoxBase):
     def onTaskTextChanged(self) -> None:
         self.yesButton.setDisabled(self.taskEdit.text().strip() == "")
 
-    def showEvent(self, event) -> None:
+    def showEvent(self, event: QShowEvent) -> None:
         super().showEvent(event)
         self.taskEdit.setFocus()
 
-    def keyPressEvent(self, event) -> None:
+    def keyPressEvent(self, event: QKeyEvent) -> None:
         if event.key() in [Qt.Key_Return, Qt.Key_Enter] and self.yesButton.isEnabled():
             self.yesButton.click()
         else:
