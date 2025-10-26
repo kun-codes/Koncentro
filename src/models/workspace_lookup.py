ed50e23d9249f7d4846a8f745677bac6389c2ab5
Bishwa Saha <77796630+kun-codes@users.noreply.github.com>
2025-10-26 23:59:10 +0530

    refactor(workspace_lookup.py): remove unused `get_current_workspace_name()`

--- src/models/workspace_lookup.py
+++ src/models/workspace_lookup.py
@@ -12,13 +12,6 @@ class WorkspaceLookup:
         return current_workspace.current_workspace_id if current_workspace else None
 
     @staticmethod
-    def get_current_workspace_name() -> Optional[str]:
-        current_workspace_id = WorkspaceLookup.get_current_workspace_id()
-        with get_session(is_read_only=True) as session:
-            workspace = session.get(Workspace, current_workspace_id)
-        return workspace.workspace_name if workspace else None
-
-    @staticmethod
     def get_current_workspace() -> Optional[Workspace]:
         current_workspace_id = WorkspaceLookup.get_current_workspace_id()
         with get_session(is_read_only=True) as session:
