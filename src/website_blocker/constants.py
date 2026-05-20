9daa60f5a740e1c8a40c4f05993c9ac0606f244f
Bishwa Saha <77796630+kun-codes@users.noreply.github.com>
2026-05-20 20:15:39 +0530

    chore(constants): remove unused MITMDUMP_SHUTDOWN_URL constant

--- src/website_blocker/constants.py
+++ src/website_blocker/constants.py
@@ -1,8 +1,5 @@
-# Don't add any 3rd party imports here, as this file is used by mitmdump directly through block.py
-
 APPLICATION_NAME = "Koncentro"
 
-MITMDUMP_SHUTDOWN_URL = f"http://shutdown.{APPLICATION_NAME.lower()}.internal/"
 BLOCK_HTML_MESSAGE = f"<h1>Website blocked by {APPLICATION_NAME}!</h1>"
 
 MITMDUMP_CHECK_URL = f"http://check.{APPLICATION_NAME.lower()}.internal/"
